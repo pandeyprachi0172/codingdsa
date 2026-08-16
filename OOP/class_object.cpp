8fd29228c9d38ace7df2a8e227980f6cad5e8824
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-16 20:40:27 +0530

    changes

--- OOP/class_object.cpp
+++ OOP/class_object.cpp
@@ -25,31 +25,3 @@ int main(){
 
 
 }
-
-#include <iostream>
-using namespace std;
-class Student{
-    public:
-    string name;
-    int age ,roll_number;
-    string grade;
-};
-
-
-int main(){
-    Student S1;
-    S1.name="Rohit";
-    S1.age=10;
-    S1.roll_number=21;
-    S1.grade="A+";
-    cout<<S1.age<< " ";
-
-    Student S2 ;
-    S2.name="Mohit";
-    S2.age=11;
-    S2.grade="b+";
-    cout <<S2.name<< "";
-    
-
-
-}
